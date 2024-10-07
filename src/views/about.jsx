3b53c038aa9e0f8d198fa09da59b457829a0f61d
Aviad_Malikan <aviad879@gmail.com>
2024-10-07 18:15:22 +0300

    change the about us text

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,9 +1,9 @@
-export function About(){
+export function About() {
 
 
     return <main>
         <h2>About US</h2>
-         <p>A Good platform that helps you unite everything you will need.
-            We have an area where you can send thoughtful and even funny emails. And another area where you will group everything you want to save such as notes, videos and lists.  </p>
+        <p>A Good platform that helps you unite everything you will need.
+            We have an area where you can send thoughtful and even funny emails. And another area where you will group everything you want to save such as notes, videos and even TODO-lists.  </p>
     </main>
 }
