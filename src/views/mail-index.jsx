0fd259be37147d6c5b10dec2eeb84dec779cc75f
Aviad_Malikan <aviad879@gmail.com>
2024-10-07 18:43:17 +0300

    mail-index load mails function,

--- src/views/mail-index.jsx
+++ src/views/mail-index.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+import { mailService } from "../services/mail.service";
 
-export function MailIndex(){
+export function MailIndex() {
+    const [mails, setMails] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
+    useEffect(() => {
+        loadMails()
+        setIsLoading(true)
+    }, [])
 
-    return <main>
-        <h1>hello mails</h1>
+    function loadMails() {
+        mailService.query().then((mailsToSet) => {
+            setMails(mailsToSet)
+            setIsLoading(false)
+        })
+    }
+
+    return <main className="">
+
+        <h1>Wemail</h1>
+        {isLoading && <h3>LOADING...</h3>}
+        {/* {(!isLoading) && JSON.stringify(mails)} */}
+        {(!mails.length && !isLoading) && <h3>No mails yet.</h3>}
     </main>
 }
