a5fa35586a5751e2599feea31e837f4ffb2dc61b
Aviad_Malikan <aviad879@gmail.com>
2024-10-09 15:27:54 +0300

    add  Route to specific mail by ID

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './assets/style/index.css';
 import { AppHeader } from './views/app-header.jsx';
 import { Home } from './views/home.jsx';
 import { About } from './views/about.jsx';
-import { MailIndex } from './views/mail-index.jsx';
 import { UserMsg } from './cmps/user-msg.jsx';
+import { MailIndex } from './views/mail-index.jsx';
+import { MailDetails } from './cmps/mails/mail-details.jsx';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/mail" element={<MailIndex />} />
+        <Route path="/mail/:mailId" element={<MailDetails />} />
       </Routes>
 
       <UserMsg />
