175dc2e6455a793e6e618c0a37acb256b8434a33
Aviad_Malikan <aviad879@gmail.com>
2024-10-14 18:12:55 +0300

    change the mail format from table to sections

--- src/cmps/mails/mail-list.jsx
+++ src/cmps/mails/mail-list.jsx
@@ -2,12 +2,28 @@ import { MailPreview } from "../mails/mail-preview";
 
 export function MailList({ mails }) {
 
-    return <table>
-        <tbody>
-            {mails.map(m => <tr key={m.id}>
-                <MailPreview mail={m} />
-            </tr>
-            )}
-        </tbody>
-    </table>
+    return <section className="mail-list">
+        {mails.map(m => <section className="mail-preview" key={m.id}>
+            <MailPreview mail={m} />
+        </section>
+        )}
+    </section>
+
+    // return <section className="mails-list">
+    //     {/* <MailPreview mail={mails[1]} /> */}
+
+    //     <div className="fav-btn pointer">⭐</div>
+    //     <div className="subject pointer">{mail.subject}</div>
+    //     <div className="mail-txt">{mail.body.txt}</div>
+    //     <div className="date">{mail.sentAt}</div>
+    // </section>
+    // return
+
+    // {
+    //     mails.map(m => <section key={m.id} >
+    //         <MailPreview mail={m} />
+    //     </section>
+    //     )
+    // }
+
 }
