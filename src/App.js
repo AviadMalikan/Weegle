2c7ecc2702dee64661be9a81a2f8b7275b6cc0c5
Aviad_Malikan <aviad879@gmail.com>
2024-11-06 15:37:36 +0200

    change the route ti sepecific one. and start to build compose-cmp.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import './assets/style/index.css';
 import { AppHeader } from './views/app-header.jsx';
@@ -18,8 +18,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/mail" element={<MailIndex />}>
-          <Route path="/mail/:label" element={<MailIndex />} />
+          {/* <Route path="/mail/inbox/compose" element={<MailIndex />} /> */}
+          <Route path="/mail/:label" element={<Navigate to="/mail/inbox" />} />
+          {/* <Route path="/mail/read" element={<Navigate to="/mail/inbox" />} /> */}
+          {/* <Route path="/mail/favorite" element={<Navigate to="/mail/inbox" />} /> */}
+          {/* <Route path="/mail/archive" element={<Navigate to="/mail/inbox" />} /> */}
         </Route>
+
+
+        {/* <Route path="/mail/*" element={<Navigate to="/mail/inbox" />} /> */}
         <Route path="/mail/inbox/:mailId" element={<MailDetails />} />
       </Routes>
 
