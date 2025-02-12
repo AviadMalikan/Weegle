ae7a723d5de50bd630a66f3fa4c5a210dadcb961
Aviad_Malikan <aviad879@gmail.com>
2025-02-13 00:25:16 +0200

    add remove options to note

--- src/views/note-index.jsx
+++ src/views/note-index.jsx
@@ -20,13 +20,19 @@ export function NoteIndex() {
         })
     }
 
+    function onRemoveNote(noteId) {
+        noteService.remove(noteId)
+            .then(() => {
+                const updateNotes = notes.filter(n =>  n.id !== noteId )
+                setNotes(updateNotes)
+            })
+    }
 
     return <main className="main-layout full">
         {isLoading && <Loader />}
 
-
         {(!notes.length && !isLoading) && <h3 className="note-list">No Notes Yet</h3>}
-        <NoteList notes={notes} />
+        <NoteList notes={notes} onRemoveNote={onRemoveNote} />
 
     </main>
 }
