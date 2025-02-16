6df772c2ca58968587e317e5f30500d7ea47fc2c
Aviad_Malikan <aviad879@gmail.com>
2025-02-16 04:16:02 +0200

    make the css of the inputs. use the state to show 2 diffrent inputs.

--- src/cmps/notes/note-edit.jsx
+++ src/cmps/notes/note-edit.jsx
@@ -3,9 +3,28 @@ import { useState } from "react"
 
 export function NoteEdit() {
     const [isInputOpen, setIsInputOpen] = useState(false)
+    const [isPinned, setIsPinned] = useState(false)
 
 
-    return <section className="note-edit">
+
+    if (isInputOpen) return <section className="note-edit">
+        <div className="add-note-container" >
+            <input type="text" name="=title" placeholder="Title" className="note-edit-title" autoComplete="off" />
+            <textarea type="text" name="=text" placeholder="Write a note" className="note-edit-text" autoComplete="off" />
+
+            <div className="note-btns">
+                <div className="add-note-btn-container">
+                    <button className="note-btn">📌</button>
+                    <button className="note-btn">🖌️</button>
+                    <button className="note-btn">🖼️</button>
+                </div>
+                <button>SAVE</button>
+            </div>
+        </div>
+        <div className="add-note-bg" onClick={() => setIsInputOpen(false)}></div>
+    </section>
+
+    else return <section className="note-edit">
         <div className="add-note-btn" onClick={() => setIsInputOpen(true)}>
             <p>Write note...</p>
             <div className="add-note-btn-container">
